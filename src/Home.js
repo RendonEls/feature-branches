417c6c6e383ac24aefa40334146faaf0783668df
Jake Brackney <brackney.jake@gmail.com>
2019-02-15 15:56:51 -0500

    Paragraph added to Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
-  
+
 
   render() {
     return (
       <div>
         <h1>Home</h1>
+        <p>This isn't frustrating at all.</p>
       </div>
     );
   }
